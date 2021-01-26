7eeaf654475282ce7065861a8d83d4d6d9d13d11
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-01-26 15:34:10 +0000

    Add basic power saving functions

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,13 +10,42 @@ describe("Thermostat", function() {
   });
 
   it("increases the temperature by 5", function() {
-    thermostat.increaseBy(5)
+    thermostat.increaseBy(5);
     expect(thermostat.temp()).toBe(25);
   });
 
   it("decreases the temperature by 5", function() {
-    thermostat.decreaseBy(5)
+    thermostat.decreaseBy(5);
     expect(thermostat.temp()).toBe(15);
+  });
+
+  it("doesn't let the temperature go below 10 degrees", function() {
+    thermostat.decreaseBy(15);
+    expect(thermostat.temp()).toBe(10);
+  });
+
+  it("sets power saving mode on when created", function(){
+    expect(thermostat.isPowerSavingOn()).toBe(true);
+  });
+
+  it("can turn on/off power saving mode", function(){
+    thermostat.turnOffPowerSaving()
+    expect(thermostat.isPowerSavingOn()).toBe(false);
+    thermostat.turnOnPowerSaving()
+    expect(thermostat.isPowerSavingOn()).toBe(true);
+  });
+
+  it("if the power saving mode is on, max temp = 25", function(){
+    thermostat.increaseBy(10);
+    expect(thermostat.temp()).toBe(25);
+  });
+
+  it("if the power saving mode is off, max temp = 32", function(){
+    thermostat.turnOffPowerSaving();
+    thermostat.increaseBy(10);
+    expect(thermostat.temp()).toBe(30);
+    thermostat.increaseBy(10);
+    expect(thermostat.temp()).toBe(32);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -2,11 +2,24 @@ class Thermostat {
 
   constructor() {
     this.temperature = 20;
+    this.max_temp = 25;
+    this.min_temp = 10;
   }
 
   temp(){ return this.temperature; }
 
-  increaseBy(num){ this.temperature += num }
-  decreaseBy(num){ this.temperature -= num }
+  isPowerSavingOn() { return (this.max_temp == 25 ? true : false ); }
+  turnOnPowerSaving() { this.max_temp = 25; }
+  turnOffPowerSaving() { this.max_temp = 32; }
+
+  increaseBy(num){ 
+    this.temperature += num;
+    if (this.temperature > this.max_temp ) { this.temperature = this.max_temp; };
+  }
+  
+  decreaseBy(num){
+    this.temperature -= num;
+    if (this.temperature < this.min_temp) {this.temperature = this.min_temp};
+  }
 
 }
